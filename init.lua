fdaf20de97af348580ee000ebea98951b7a54fa2
chriseng2023 <126883051+chriseng2023@users.noreply.github.com>
2025-09-01 10:07:21 +0800

    Add files via upload
    The initial files are comes from jdhao, I modify and keep basic function, it's good for start.

---
+++ init.lua
@@ -0,0 +1,31 @@
+vim.loader.enable()
+
+-- local utils = require("utils")
+
+-- local expected_version = "0.11.2"
+-- utils.is_compatible_version(expected_version)
+
+local config_dir = vim.fn.stdpath("config")
+---@cast config_dir string
+
+-- some global settings
+require("globals")
+-- setting options in nvim
+vim.cmd("source " .. vim.fs.joinpath(config_dir, "viml_conf/options.vim"))
+-- various autocommands
+-- require("custom-autocmd")
+-- 
+-- all the user-defined mappings
+-- require("mappings")
+--
+-- all the plugins installed and their configurations
+-- vim.cmd("source " .. vim.fs.joinpath(config_dir, "viml_conf/plugins.vim"))
+
+-- diagnostic related config
+-- require("diagnostic-conf")
+
+-- colorscheme settings
+-- local color_scheme = require("colorschemes")
+
+-- Load a random colorscheme
+-- color_scheme.rand_colorscheme()
